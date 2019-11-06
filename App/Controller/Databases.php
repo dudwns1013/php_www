39c80c6e8de1479531eee1e1cd3e58414b338a89
dudwns1013 <dudwns1013@gmail.com>
2019-11-06 11:22:06 +0900

    m

--- App/Controller/Databases.php
+++ App/Controller/Databases.php
@@ -2,37 +2,37 @@
 
 namespace App\Controller;
 
-class TableInfo{
+class Databases{
     private $db;
     // 생성자
     public function __construct($db){
-        //echo __CLASS__;
+        // echo __CLASS__;
         $this->db=$db;
     }
 
     public function main(){
-        //echo "메인 호출이에요.";
+        
         $html = new \Module\Html\HtmlTable;
-        $uri = $_SERVER['REQUEST_URI'];
-        $uris = explode("/",$uri); // 파란책
 
-        $query = "DESC ".$uris[2];
+        $query = "SHOW Databases";
         $result = $this->db->queryExecute($query);
-
         $count = mysqli_num_rows($result);  
         $content =""; // 초기화
         $rows = []; // 배열 초기화
+
         for($i=0;$i<$count;$i++){
-            $row =mysqli_fetch_object($result);
-            //print_r($row);
-            //foreach ($row as $r) {
-            //    echo $r." ";
-            //}
-            $rows []= $row; // 배열 추가 (2차원 배열)
+            $row = mysqli_fetch_object($result);
+            // $rows []=$row; // 배열 추가
+            $rows []=[
+                // 배열 * 배열 = 2차원 배열.
+                // 키와 값을 가지는 연상 배열
+                'num' =>$i,
+                'name'=>"<a href='/Tables/".$row->Database."'>".$row->Database."</a>"
+            ];
         }
         $content = $html->table($rows);
 
-        $body = file_get_contents("../Resource/desc.html");
+        $body = file_get_contents("../Resource/database.html");
         $body = str_replace("{{content}}",$content,$body); // 데이터 치환
         echo $body;
     }
